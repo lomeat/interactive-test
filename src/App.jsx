094e9574da25bb063307883aeb987f9b2e1203ef
lomeat <lom3at@gmail.com>
2020-07-28 03:44:24 +0300

    add: route pages

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+
+import { Home } from './components/Home';
+import { Favourite } from './containers/Favourite';
+
+export const App = props => {
+  return (
+    <Router>
+      <Navbar>
+        <StLink to="/">Home</StLink>
+        <StLink to="/favourite">Favourite</StLink>
+      </Navbar>
+
+      <Wrapper>
+        <Container>
+          <Switch>
+            <Route path="/favourite">
+              <Favourite />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+      </Wrapper>
+    </Router>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 1000px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  padding: 30px 0;
+`;
+
+const Navbar = styled.div`
+  width: 100vw;
+  background: #eee;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const StLink = styled(Link)`
+  margin-left: 20px;
+  :first-child {
+    margin: 0;
+  }
+`;
--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { init } from '@rematch/core';
 import 'normalize.css';
+import { createGlobalStyle } from 'styled-components';
 
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
+`;
 
 const store = init();
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyle />
     <React.StrictMode>
       <App />
     </React.StrictMode>
